dd0233e583419d3e8cb52fd2a6f98966ce271177
RohitDayal <rdxdayal35@gmail.com>
2024-07-09 03:18:28 +0530

    minor issue

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const createTables = require("./models/createTables");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 // app.use((req, res, next) => {
 //   console.log(`${req.method} request for ${req.url}`);
 //   next();
@@ -27,39 +27,9 @@ app.use("/api", dataServices);
 app.use("/create-saloon", createSaloon);
 app.use("/users", usersRoute);
 
-// const razorpay = new Razorpay({
-//   key_id: "rzp_test_ckSYAx32pq1y0r",
-//   key_secret: "2XXpJvVqrv3IZbfvCDzXp0Ws",
-// });
-
-app.post("/api/payment/create", async (req, res) => {
-  const payment_capture = 1;
-  const amount = req.body.amount; // Amount in paise
-  const currency = "INR";
-
-  const options = {
-    amount: amount * 100,
-    currency,
-    receipt: "receipt_order_74394",
-    payment_capture,
-  };
-
-  try {
-    const response = await razorpay.orders.create(options);
-    res.json({
-      id: response.id,
-      currency: response.currency,
-      amount: response.amount,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Some error occurred");
-  }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
 
 // Ensure tables are created before starting the server
 const port = process.env.PORT || 5000;
